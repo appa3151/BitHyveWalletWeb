806393c3a974b0ae5bfd496f4341f642a7c56fc2
appa3151 <onlyappasaheb4@gmail.com>
2019-02-28 13:03:07 +0530

    Updates

---
+++ precache-manifest.b1b2e884e2a52763fa3894d3ee172ae2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b42659a00b8ca08c283",
+    "url": "/bithyve/static/js/runtime~main.8b42659a.js"
+  },
+  {
+    "revision": "8dceb2ac62e7a5655865",
+    "url": "/bithyve/static/js/main.8dceb2ac.chunk.js"
+  },
+  {
+    "revision": "53341b06a5b8e5594afa",
+    "url": "/bithyve/static/js/1.53341b06.chunk.js"
+  },
+  {
+    "revision": "8dceb2ac62e7a5655865",
+    "url": "/bithyve/static/css/main.530fa9b1.chunk.css"
+  },
+  {
+    "revision": "0c4deb149ba9ef1dd56c4e4146c4132a",
+    "url": "/bithyve/index.html"
+  }
+];
