9920000ecdedd65a987107e0d0ecaf1ca8157bec
appa3151 <onlyappasaheb4@gmail.com>
2019-02-28 14:18:14 +0530

    slider image set

--- src/screen/views/HomeScreen/HomeScreen.tsx
+++ src/screen/views/HomeScreen/HomeScreen.tsx
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import { Button, Container, Row, Col } from "reactstrap";
 
 export default class HomeScreen extends Component {
   render() {
     return (
-      <div className="App" style={{ textAlign: "center" }}>
-        <h1>Bithyve Wallet</h1>
-      </div>
+      <Container fluid>
+        <div className="App" style={{ textAlign: "center" }}>
+          <div>
+            <h1>SOUND MONEY. SIMPLIFIED</h1>
+            <span>ENABLED BY A P2P NETWORK OF NON-CUSTODIAL WALLETS</span>
+          </div>
+          <div>
+            <img
+              style={{ height: 410, width: "90%", marginTop: 20 }}
+              src={require("../../../assets/images/logosaving.jpeg")}
+            />
+          </div>
+        </div>
+      </Container>
     );
   }
 }
