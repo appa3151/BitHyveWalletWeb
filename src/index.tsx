e7cc112e45b3a8e88966e27fb3b90e507c55208a
appa3151 <onlyappasaheb4@gmail.com>
2019-02-28 16:57:48 +0530

    router set

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import HomeScreen from "./screen/views/HomeScreen/HomeScreen";
+
+import { HashRouter, Switch, Route, BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<HomeScreen />, document.getElementById("root"));
+export default class BitHyveWalletWeb extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={HomeScreen} exact />
+          <Route path="/" component={HomeScreen} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+ReactDOM.render(<BitHyveWalletWeb />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
